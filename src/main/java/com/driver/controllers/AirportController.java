06c8c1e8a2681095a0e9f8a0683270d24ec35c0b
Ujjwalb09 <ujjwalbhatt09@gmail.com>
2023-05-25 16:38:00 +0530

    done7

--- src/main/java/com/driver/controllers/AirportController.java
+++ src/main/java/com/driver/controllers/AirportController.java
@@ -114,6 +114,11 @@ public class AirportController {
         //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
         //This will not include the current person who is trying to book, he might also be just checking price
 
+        if(Objects.isNull(ticketDb.get(flightId).size())){
+            return 0;
+        }
+
+
         int noOfPeopleBooked = ticketDb.get(flightId).size();
 
         return noOfPeopleBooked*50 + 3000;
